3a6807a53163b7a4a6260949145467422a13dd42
Dinusha Chathuranga <dinusha.21@cse.mrt.ac.lk>
2025-11-26 12:06:07 +0530

    key changed

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "new-key.pem" 
+  key_name      = "new-key" 
   tags = {
     Name = var.instance_name
   }
